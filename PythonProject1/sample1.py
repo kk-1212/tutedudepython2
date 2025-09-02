21b90def90e8fe00cb808a222c6bef2d4eb8b50e
kk-1212 <109754475+kk-1212@users.noreply.github.com>
2025-09-02 18:05:22 +0530

    Delete PythonProject1 directory

--- PythonProject1/sample1.py
+++
@@ -1,9 +0,0 @@
-#Task 1:
-try:
-    print("Reading the file content")
-    f = open("sample.txt")
-    f1 = f.readlines()
-    print("Line 1:- ", f1[0], end='')
-    print("Line 2:- ", f1[1])
-except FileNotFoundError:
-    print("File "'sample.txt'" not found")
--- PythonProject1/testing.py
+++
@@ -1,4 +0,0 @@
-# with open ('sample.txt', 'r+') as f:
-#     e = f.readlines(1)
-#     print (e)
-# f.2close()
